0b26f338c6b606a5617e8273916407ef5b69da01
mastergenius <mastergenius@yandex.ru>
2016-09-22 16:17:11 +0300

    Dirty hack: inline wildemmiter to workaround strange require bug

--- index.jsx
+++ index.jsx
@@ -5,7 +5,8 @@ var _ = {
 }
 var React = require('react')
 var DataFrame = require('dataframe')
-var Emitter = require('wildemitter')
+// Hack: inline wildemitter
+// var Emitter = require('wildemitter')
 
 var partial = require('./lib/partial')
 var download = require('./lib/download')
@@ -13,6 +14,143 @@ var getValue = require('./lib/get-value')
 var PivotTable = require('./lib/pivot-table.jsx')
 var Dimensions = require('./lib/dimensions.jsx')
 var ColumnControl = require('./lib/column-control.jsx')
+
+// Hack: inline wildemitter
+function WildEmitter() { }
+
+WildEmitter.mixin = function (constructor) {
+  var prototype = constructor.prototype || constructor;
+
+  prototype.isWildEmitter= true;
+
+  // Listen on the given `event` with `fn`. Store a group name if present.
+  prototype.on = function (event, groupName, fn) {
+    this.callbacks = this.callbacks || {};
+    var hasGroup = (arguments.length === 3),
+        group = hasGroup ? arguments[1] : undefined,
+        func = hasGroup ? arguments[2] : arguments[1];
+    func._groupName = group;
+    (this.callbacks[event] = this.callbacks[event] || []).push(func);
+    return this;
+  };
+
+  // Adds an `event` listener that will be invoked a single
+  // time then automatically removed.
+  prototype.once = function (event, groupName, fn) {
+    var self = this,
+        hasGroup = (arguments.length === 3),
+        group = hasGroup ? arguments[1] : undefined,
+        func = hasGroup ? arguments[2] : arguments[1];
+    function on() {
+      self.off(event, on);
+      func.apply(this, arguments);
+    }
+    this.on(event, group, on);
+    return this;
+  };
+
+  // Unbinds an entire group
+  prototype.releaseGroup = function (groupName) {
+    this.callbacks = this.callbacks || {};
+    var item, i, len, handlers;
+    for (item in this.callbacks) {
+      handlers = this.callbacks[item];
+      for (i = 0, len = handlers.length; i < len; i++) {
+        if (handlers[i]._groupName === groupName) {
+          //console.log('removing');
+          // remove it and shorten the array we're looping through
+          handlers.splice(i, 1);
+          i--;
+          len--;
+        }
+      }
+    }
+    return this;
+  };
+
+  // Remove the given callback for `event` or all
+  // registered callbacks.
+  prototype.off = function (event, fn) {
+    this.callbacks = this.callbacks || {};
+    var callbacks = this.callbacks[event],
+        i;
+
+    if (!callbacks) return this;
+
+    // remove all handlers
+    if (arguments.length === 1) {
+      delete this.callbacks[event];
+      return this;
+    }
+
+    // remove specific handler
+    i = callbacks.indexOf(fn);
+    callbacks.splice(i, 1);
+    if (callbacks.length === 0) {
+      delete this.callbacks[event];
+    }
+    return this;
+  };
+
+  /// Emit `event` with the given args.
+  // also calls any `*` handlers
+  prototype.emit = function (event) {
+    this.callbacks = this.callbacks || {};
+    var args = [].slice.call(arguments, 1),
+        callbacks = this.callbacks[event],
+        specialCallbacks = this.getWildcardCallbacks(event),
+        i,
+        len,
+        item,
+        listeners;
+
+    if (callbacks) {
+      listeners = callbacks.slice();
+      for (i = 0, len = listeners.length; i < len; ++i) {
+        if (!listeners[i]) {
+          break;
+        }
+        listeners[i].apply(this, args);
+      }
+    }
+
+    if (specialCallbacks) {
+      len = specialCallbacks.length;
+      listeners = specialCallbacks.slice();
+      for (i = 0, len = listeners.length; i < len; ++i) {
+        if (!listeners[i]) {
+          break;
+        }
+        listeners[i].apply(this, [event].concat(args));
+      }
+    }
+
+    return this;
+  };
+
+  // Helper for for finding special wildcard event handlers that match the event
+  prototype.getWildcardCallbacks = function (eventName) {
+    this.callbacks = this.callbacks || {};
+    var item,
+        split,
+        result = [];
+
+    for (item in this.callbacks) {
+      split = item.split('*');
+      if (item === '*' || (split.length === 2 && eventName.slice(0, split[0].length) === split[0])) {
+        result = result.concat(this.callbacks[item]);
+      }
+    }
+    return result;
+  };
+
+};
+
+WildEmitter.mixin(WildEmitter);
+
+var PivotEmmiter = function () {};
+WildEmitter.mixin(PivotEmmiter);
+
 
 module.exports = React.createClass({
   displayName: 'ReactPivot',
@@ -31,7 +169,7 @@ module.exports = React.createClass({
       hiddenColumns: [],
       sortBy: null,
       sortDir: 'asc',
-      eventBus: new Emitter,
+      eventBus: new PivotEmmiter(),
       compact: false,
       excludeSummaryFromExport: false,
       onData: function () {}
@@ -68,7 +206,7 @@ module.exports = React.createClass({
 
     this.updateRows()
   },
-  
+
   componentWillReceiveProps: function(newProps) {
     if(newProps.rows !== this.props.rows) {
       this.dataFrame = DataFrame({
@@ -76,7 +214,7 @@ module.exports = React.createClass({
         dimensions: this.props.dimensions,
         reduce: this.props.reduce
       })
-      
+
       this.updateRows()
     }
   },
@@ -110,45 +248,45 @@ module.exports = React.createClass({
 
   render: function() {
     var html = (
-      <div className='reactPivot'>
+        <div className='reactPivot'>
 
-      { this.props.hideDimensionFilter ? '' :
-        <Dimensions
-          dimensions={this.props.dimensions}
-          selectedDimensions={this.state.dimensions}
-          onChange={this.setDimensions} />
-      }
+          { this.props.hideDimensionFilter ? '' :
+              <Dimensions
+                  dimensions={this.props.dimensions}
+                  selectedDimensions={this.state.dimensions}
+                  onChange={this.setDimensions} />
+          }
 
-        <ColumnControl
-          hiddenColumns={this.state.hiddenColumns}
-          onChange={this.setHiddenColumns} />
+          <ColumnControl
+              hiddenColumns={this.state.hiddenColumns}
+              onChange={this.setHiddenColumns} />
 
-        <div className="reactPivot-csvExport">
-          <button onClick={partial(this.downloadCSV, this.state.rows)}>
-            Export CSV
-          </button>
-        </div>
+          <div className="reactPivot-csvExport">
+            <button onClick={partial(this.downloadCSV, this.state.rows)}>
+              Export CSV
+            </button>
+          </div>
 
-        { !this.state.solo ? '' :
-          <div style={{clear: 'both'}} className='reactPivot-soloDisplay'>
+          { !this.state.solo ? '' :
+              <div style={{clear: 'both'}} className='reactPivot-soloDisplay'>
             <span className='reactPivot-clearSolo' onClick={this.clearSolo}>
               &times;
             </span>
-            {this.state.solo.title}: {this.state.solo.value}
-          </div>
-        }
+                {this.state.solo.title}: {this.state.solo.value}
+              </div>
+          }
 
-        <PivotTable
-          columns={this.getColumns()}
-          rows={this.state.rows}
-          sortBy={this.state.sortBy}
-          sortDir={this.state.sortDir}
-          onSort={this.setSort}
-          onColumnHide={this.hideColumn}
-          nPaginateRows={this.props.nPaginateRows}
-          onSolo={this.setSolo} />
+          <PivotTable
+              columns={this.getColumns()}
+              rows={this.state.rows}
+              sortBy={this.state.sortBy}
+              sortDir={this.state.sortDir}
+              onSort={this.setSort}
+              onColumnHide={this.hideColumn}
+              nPaginateRows={this.props.nPaginateRows}
+              onSolo={this.setSolo} />
 
-      </div>
+        </div>
     )
 
     return html
@@ -159,8 +297,8 @@ module.exports = React.createClass({
 
     var sortByTitle = this.state.sortBy
     var sortCol = _.find(columns, function(col) {
-      return col.title === sortByTitle
-    }) || {}
+          return col.title === sortByTitle
+        }) || {}
     var sortBy = sortCol.type === 'dimension' ? sortCol.title : sortCol.value
     var sortDir = this.state.sortDir
 
@@ -224,7 +362,8 @@ module.exports = React.createClass({
   },
 
   hideColumn: function(cTitle) {
-    var hidden = this.state.hiddenColumns.concat([cTitle])
+    var hidden = this.state.hiddenColumns
+    hidden.push(cTitle)
     this.setHiddenColumns(hidden)
     setTimeout(this.updateRows, 0)
   },
@@ -235,8 +374,8 @@ module.exports = React.createClass({
     var columns = this.getColumns()
 
     var csv = _.map(columns, 'title')
-      .map(JSON.stringify.bind(JSON))
-      .join(',') + '\n'
+            .map(JSON.stringify.bind(JSON))
+            .join(',') + '\n'
 
     var maxLevel = this.state.dimensions.length - 1
     var excludeSummary = this.props.excludeSummaryFromExport
